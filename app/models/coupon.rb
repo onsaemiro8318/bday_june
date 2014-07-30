ca2ed55472d15007806a2276707b546db80e013c
onesup <onesup@test.net>
2014-07-30 15:46:15 +0900

    통계

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,7 +3,93 @@ class Coupon < ActiveRecord::Base
   
   scope :used, -> { where status: 'used' }
   scope :not_used, -> { where status: 'not_used' }
+  def self.weekday
+    Coupon.select(
+      "sum(case when DayofWeek(convert_tz(coupons.updated_at,'+00:00','+09:00')) = 1 and status='used' then 1 else 0 end) as 'sun'
+      ,sum(case when DayofWeek(convert_tz(coupons.updated_at,'+00:00','+09:00')) = 2 and status='used' then 1 else 0 end) as 'mon'
+      ,sum(case when DayofWeek(convert_tz(coupons.updated_at,'+00:00','+09:00')) = 3 and status='used' then 1 else 0 end) as 'tue'
+      ,sum(case when DayofWeek(convert_tz(coupons.updated_at,'+00:00','+09:00')) = 4 and status='used' then 1 else 0 end) as 'wed'
+      ,sum(case when DayofWeek(convert_tz(coupons.updated_at,'+00:00','+09:00')) = 5 and status='used' then 1 else 0 end) as 'thu'
+      ,sum(case when DayofWeek(convert_tz(coupons.updated_at,'+00:00','+09:00')) = 6 and status='used' then 1 else 0 end) as 'fri'
+      ,sum(case when DayofWeek(convert_tz(coupons.updated_at,'+00:00','+09:00')) = 7 and status='used' then 1 else 0 end) as 'sat' ")
+  end
   
+  def self.log_file(date)
+    date = date.to_s
+    lines = []
+    file = File.new("production02.log")
+      lines = lines + file.readlines
+    file.close
+    file = File.new("production03.log")
+      lines = lines + file.readlines
+    file.close
+    file = File.new("production04.log")
+      lines = lines + file.readlines
+    file.close
+    file = File.new("production_0729.log")
+      lines = lines + file.readlines
+    file.close
+    greps = lines.grep(/PUT.\"\/[a-z]{5}\-[0-9]{4}/)
+    greps
+    codes = []
+    greps.each_with_index do |grep, i|
+      log_time = grep.partition("INFO --").first
+      updated_at = DateTime.parse(log_time).change(offset:"+0900")
+      start_time = DateTime.parse "2014-07-"+date+" 00:00:00 +0900"
+      finish_time = DateTime.parse "2014-07-"+date+" 23:59:59 +0900"
+      if start_time < updated_at and updated_at > finish_time
+        codes << {code: grep[/[a-z]{5}\-[0-9]{4}/], updated_at: updated_at}
+      end
+    end
+    coupons = []
+    codes.each do |code|
+      coupon = Coupon.find_by_code(code[:code])
+      unless coupon.nil?
+        coupon.status = "used"
+        coupon.updated_at = code[:updated_at]
+        coupons << coupon.save
+      end
+    end
+    coupons
+  end
+  
+  
+  
+  # def self.log_file(day)
+  #   lines = []
+  #   file = File.new("production08.log")
+  #     lines = lines + file.readlines
+  #   file.close
+  #   file = File.new("production09.log")
+  #     lines = lines + file.readlines
+  #   file.close
+  #   file = File.new("production10.log")
+  #     lines = lines + file.readlines
+  #   file.close
+  #   greps = lines.grep(/, "user"=>{"b/)
+  #   users = []
+  #   greps.each_with_index do |grep, i|
+  #     hash = eval grep.partition("ters: ").last
+  #     log_time = grep.partition("INFO --").first
+  #     created_at = DateTime.parse(log_time).change(offset:"+0900")
+  #     user = hash["user"]
+  #     user["created_at"] = created_at
+  #     start_time = DateTime.parse("2014-07-"+day.to_s+" 00:00:00 +0900")
+  #     finish_time = DateTime.parse("2014-07-"+day.to_s+" 23:59:59 +0900")
+  #     if start_time <= created_at and created_at <= finish_time
+  #       u = User.create(user)
+  #       coupon = Coupon.new
+  #       coupon.code = coupon.random_code
+  #       coupon.user = u
+  #       coupon.save
+  #       users << u
+  #     end
+  #   end
+  #   users
+  # end
+  
+
+
   def send_message
     Message.send_to(self)
   end
